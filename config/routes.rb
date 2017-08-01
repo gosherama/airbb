998dc9289fa1321a961dbc1cf3e48e762e1a77c2
gosher <macciak@gmail.com>
2017-08-01 13:00:17 +0000

    Install devise and locale FR resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, :path => '',
+                    :path_names => { :sign_in => 'Login', :sign_out => 'Logout', :edit => 'Profile'}
   root 'pages#home'
 end
