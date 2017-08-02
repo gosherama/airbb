9afea8a8f6a662d573c0aeaf7ea96e00554b3ce8
gosher <macciak@gmail.com>
2017-08-02 07:08:54 +0000

    Added User show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users, :path => '',
                     :path_names => { :sign_in => 'Login', :sign_out => 'Logout', :edit => 'Profile'}
   root 'pages#home'
+  resources :users, only: [:show]
 end
