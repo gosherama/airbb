c0ae8fbaf21506c2297b56d3aa7314836a215409
gosher <macciak@gmail.com>
2017-08-06 19:56:27 +0000

    photos model

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,15 +1,14 @@
 class PhotosController < ApplicationController
 
   def destroy
+    #correspond à l’id de la photo concernée
     @photo = Photo.find(params[:id])
     room = @photo.room
     
     @photo.destroy
-    
+    #réactualise la liste des photos de ce logement
     @photos = Photo.where(room_id: room.id)
-    
+    #pour la suppression immédiate 
     respond_to :js
-    
   end
-
 end
